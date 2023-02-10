d69b54dd85a09dbfe4642b4f6403dfd6a4b80450
Ryo Okazaki <ryo.okazaki@asia-quest.jp>
2023-02-11 03:16:32 +0900

    ・aws-sdkインストール ・SES設定

--- laravel/config/mail.php
+++ laravel/config/mail.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'ses'),
 
     /*
     |--------------------------------------------------------------------------
@@ -45,7 +45,9 @@ return [
         ],
 
         'ses' => [
-            'transport' => 'ses',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'ap-northeast-1'),
         ],
 
         'mailgun' => [
